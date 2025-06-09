53b257ea2c11fcda8d240417e876bfeed9c8536a
kingrayhan <rayhan095@gmail.com>
2025-06-09 22:46:06 +0600

    Refactor database.ts to export PgClient and rename db to database for clarity in database connection handling.

--- src/backend/database.ts
+++ src/backend/database.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schemas/schemas";
-import { Client as PgClient } from "pg";
-// export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export { Client as PgClient } from "pg";
+export const database = drizzle(process.env.DATABASE_URL!, { schema });
