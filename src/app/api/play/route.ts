5eb33af44c320dd0ec3121e79b3b8478e4980675
kingrayhan <rayhan095@gmail.com>
2025-06-19 17:46:23 +0600

    refactor: simplify GET response to return a static play status

--- src/app/api/play/route.ts
+++ src/app/api/play/route.ts
@@ -1,13 +1,11 @@
-import { myBookmarks } from "@/backend/services/bookmark.action";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const bookmarks = await myBookmarks({
-    resource_type: "ARTICLE",
-  });
-
-  return NextResponse.json(bookmarks, {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+  return NextResponse.json(
+    { play: true },
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
