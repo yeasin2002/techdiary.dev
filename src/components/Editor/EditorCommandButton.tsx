aa49ab55e033b2efe093da248c2edf3669581ab8
kingrayhan <rayhan095@gmail.com>
2025-06-24 23:51:56 +0600

    fix: correctly render Icon prop in EditorCommandButton component

--- src/components/Editor/EditorCommandButton.tsx
+++ src/components/Editor/EditorCommandButton.tsx
@@ -4,10 +4,10 @@ interface Prop extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   Icon: React.ReactNode;
   isDisabled?: boolean;
 }
-const EditorCommandButton: React.FC<Prop> = ({ isDisabled, ...props }) => {
+const EditorCommandButton: React.FC<Prop> = ({ isDisabled, Icon, ...props }) => {
   return (
     <button {...props} disabled={isDisabled} type="button">
-      {props.Icon}
+      {Icon}
     </button>
   );
 };
