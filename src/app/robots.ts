6ca7663a328394da1fe209b50049e459ccf35af2
kingrayhan <rayhan095@gmail.com>
2025-06-26 03:39:23 +0600

    refactor: update robots.ts to use array for disallowed paths and format sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,14 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
+      crawlDelay: 10,
       allow: "/",
-      disallow: "/dashboard",
+      disallow: ["/dashboard", "/backdoor"],
     },
     host: "https://www.techdiary.dev",
+    sitemap: [
+      "https://www.techdiary.dev/sitemaps/articles/sitemap.xml",
+      "https://www.techdiary.dev/sitemaps/profiles/sitemap.xml",
+    ],
   };
 }
