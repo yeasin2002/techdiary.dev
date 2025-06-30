7d1218efdc72311bfb312212e516ccd202c3ba70
kingrayhan <rayhan095@gmail.com>
2025-07-01 00:25:08 +0600

    feat: refactor UnsplashImageGallery to improve image data handling and search functionality

--- src/components/UnsplashImageGallery.tsx
+++ src/components/UnsplashImageGallery.tsx
@@ -5,7 +5,7 @@ import { actionPromisify } from "@/lib/utils";
 import { UploadIcon } from "@radix-ui/react-icons";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader } from "lucide-react";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Input } from "./ui/input";
 import ImageCropperModal from "./ImageCropperModal";
 import { useToggle } from "@/hooks/use-toggle";
@@ -24,7 +24,7 @@ const UnsplashImageGallery: React.FC<IProp> = ({
   uploadDirectory,
 }) => {
   const { _t } = useTranslation();
-  const [q, setQ] = useState("programming");
+  const [q, setQ] = useState("");
   const [selectedURL, setSelectedURL] = useState("");
   const [cropperOpened, cropperModalHandle] = useToggle();
   const debouncedSearchQuery = useDebouncedCallback((searchTerm: string) => {
@@ -33,14 +33,20 @@ const UnsplashImageGallery: React.FC<IProp> = ({
 
   const query = useInfiniteQuery({
     queryKey: ["unsplash", q],
-    queryFn: ({ pageParam }) => actionPromisify(searchUnsplash(q, pageParam)),
+    queryFn: ({ pageParam }) =>
+      actionPromisify(searchUnsplash(q || "programming", pageParam)),
     initialPageParam: 1,
     getNextPageParam: (lastPage) => {
+      console.log(lastPage.meta);
       if (!lastPage?.meta?.hasNextPage) return undefined;
       return lastPage?.meta?.currentPage + 1;
     },
-    enabled: !!q,
   });
+
+  const data = useMemo(
+    () => query.data?.pages?.flatMap((page) => page?.results || []) || [],
+    [query.data]
+  );
 
   return (
     <>
@@ -68,28 +74,31 @@ const UnsplashImageGallery: React.FC<IProp> = ({
             </div>
           )}
           <div className="max-w-full gap-5 columns-2 sm:gap-8 lg:columns-3 [&>img:not(:first-child)]:mt-8">
-            {query.data?.pages?.map((page) =>
-              page?.results?.map((image) => (
-                <div
-                  key={image.id}
-                  onClick={() => {
-                    setSelectedURL(image.urls.regular);
-                    cropperModalHandle.open();
-                  }}
-                  className="relative mb-6 rounded-lg overflow-hidden group cursor-pointer"
-                >
-                  <img src={image.urls.regular} alt={image.description!} />
-                  <div className="bg-black/45 opacity-0 absolute top-0 right-0 bottom-0 left-0 transition-all duration-300 group-hover:opacity-100 flex items-center justify-center">
-                    <UploadIcon className="w-10 h-10 z-50 text-white" />
-                  </div>
+            {data.map((image) => (
+              <div
+                key={image.id}
+                onClick={() => {
+                  setSelectedURL(image.urls.regular);
+                  cropperModalHandle.open();
+                }}
+                className="relative mb-6 rounded-lg overflow-hidden group cursor-pointer"
+              >
+                <img src={image.urls.regular} alt={image.description!} />
+                <div className="bg-black/45 opacity-0 absolute top-0 right-0 bottom-0 left-0 transition-all duration-300 group-hover:opacity-100 flex items-center justify-center">
+                  <UploadIcon className="w-10 h-10 z-50 text-white" />
                 </div>
-              ))
-            )}
+              </div>
+            ))}
           </div>
-          <VisibilitySensor
-            visible={query.hasNextPage && !query.isFetchingNextPage && query.data?.pages?.length > 0}
-            onLoadmore={() => query.fetchNextPage()}
-          />
+
+          <div className="text-center my-2">
+            <button
+              className="text-sm cursor-pointer"
+              onClick={() => query.fetchNextPage()}
+            >
+              {query.isFetchingNextPage ? "Loading..." : "Load more"}
+            </button>
+          </div>
         </div>
       </div>
     </>
