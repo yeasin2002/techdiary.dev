0c6ca594b1180eb5b8cf27e90d2ebd9f24899e73
kingrayhan <rayhan095@gmail.com>
2025-07-01 13:39:41 +0600

    refactor: remove unused fetch event listener from service worker

--- public/sw.js
+++ public/sw.js
@@ -10,16 +10,6 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// Fetch event
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      // Return cached version or fetch from network
-      return response || fetch(event.request);
-    })
-  );
-});
-
 // Activate event
 self.addEventListener("activate", (event) => {
   event.waitUntil(
